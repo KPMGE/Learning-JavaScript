7c2b95e9b1a86abf8616f8d399bcc3b67f366a6e
KPMGE <jesuskevin254@gmail.com>
2020-09-20 19:33:12 -0300

    adding example of scope in JavaScript programming language

---
+++ functions/scope.js
@@ -0,0 +1,24 @@
+// program showing block-scoped concept
+// global variable
+let a = 'Hello';
+
+function greet() {
+
+    // local variable
+    let b = 'World';
+
+    console.log(a + ' ' + b);
+
+    if (b == 'World') {
+
+        // block-scoped variable
+        let c = 'hello';
+
+        console.log(a + ' ' + b + ' ' + c);
+    }
+
+    // variable x cannot be accessed here
+    console.log(a + ' ' + b + ' ' + c);
+}
+
+greet();
