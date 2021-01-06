082f05292ed35ec898f0cea60dd1aaa26f571925
KPMGE <jesuskevin254@gmail.com>
2021-01-06 20:31:00 -0300

    Adding first exercise

---
+++ exercises-book-eloquent-javaScript/chapter2/looping-a-triangle.js
@@ -0,0 +1,10 @@
+// Adding first exercise from eloquent JavaScript - charpter 2
+
+for (let i = 0; i < 7; i++)
+{
+  for (let j = 0; j < i; j++)
+  {
+    console.log("#");
+  }
+  console.log("\n");
+}
