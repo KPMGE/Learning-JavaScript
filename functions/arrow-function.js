dfdd03e526aef0ba24526c44ffe3f323af508deb
KPMGE <jesuskevin254@gmail.com>
2021-01-06 21:04:38 -0300

    Adding example of arrow funtion

---
+++ functions/arrow-function.js
@@ -0,0 +1,10 @@
+// simple example of use arrow functions
+
+const power = (base, exponent) => {
+    let result = 1;
+
+    for (let count = 0; count < exponent; count++) {
+        result *= base;
+    }
+    return result;
+};
