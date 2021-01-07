7f07b85e78238ccbe878a84808852b1c4ca62faf
KPMGE <jesuskevin254@gmail.com>
2021-01-07 15:31:23 -0300

    Adding example with push and pop methods

---
+++ arrays/push-pop.js
@@ -0,0 +1,10 @@
+let array = [1, 2, 3]; 
+
+console.log("original array: " + array);
+
+array.push(4, 5, 6); //adding elements 4, 5 and 6
+console.log("new elements added to array: " + array);
+array.pop(); // removing least element from array
+console.log("least value removed from array: " + array);
+
+
