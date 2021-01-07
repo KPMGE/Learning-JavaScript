ad3d6883ea2aa62bec62749862965fa1c2c36c6e
KPMGE <jesuskevin254@gmail.com>
2021-01-07 10:23:46 -0300

    Adding program that counts the occurrency of a letter in a string

---
+++ exercises-book-eloquent-javaScript/chapter3/bean-counting.js
@@ -0,0 +1,17 @@
+// function that counts the letter occurrency in the string
+
+function countLetter(someStringh, letter) 
+{
+  let amount = 0;
+
+  for (let i = 0; i < someStringh.length; i++)
+  {
+    if (someStringh[i] === letter)
+      amount++;
+  }
+
+  return amount;
+}
+
+let str = "JavaScript is really awesome!";
+console.log(countLetter(str, "a"));
