fdd8fcf45a7a1d85d881c4bb41377c171fe7471e
KPMGE <jesuskevin254@gmail.com>
2021-01-07 15:26:15 -0300

    Adding example with simple operations on arrays

---
+++ arrays/simple-array.js
@@ -0,0 +1,16 @@
+// simple example of an array
+
+let array = [10, 22, 31];
+let myName = "KeVIn";
+let upperCaseName = myName.toUpperCase(); // converting to upper case
+let lowerCaseName = myName.toLowerCase(); // converting to lower case
+
+// numeric array
+console.log("The intire array: " + array);
+console.log("array in first position: " + array[0]);
+console.log("Array's size: " + array.length);
+
+// for string
+console.log("Original string: " + myName);
+console.log("my name on upper case: " + upperCaseName);
+console.log("my name on lower case: " + lowerCaseName);
