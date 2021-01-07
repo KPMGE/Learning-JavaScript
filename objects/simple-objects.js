a345fa9aed50198fefcfcfe2a7ada0ee1828c3c0
KPMGE <jesuskevin254@gmail.com>
2021-01-07 14:49:25 -0300

    Adding example using a simple object

---
+++ objects/simple-objects.js
@@ -0,0 +1,15 @@
+// example of a simple object
+
+let weekAssignments = {
+  sunday: "work out at 6AM",
+  monday: "run",
+  tuesday: "wash the dishes", 
+  wednesday: "crying",
+  thursday: "watch a movie",
+  friday: "go to my brother's party",
+  saturday: "crying because anything wants me"
+};
+
+console.log(weekAssignments);
+console.log("i'll " + weekAssignments.friday + " on friday");
+console.log("and then, on saturday: " + weekAssignments.saturday);
