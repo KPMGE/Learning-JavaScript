d7498fec9a4db98d560192b94995686804917e08
KPMGE <jesuskevin254@gmail.com>
2021-01-07 09:55:10 -0300

    Adding version of function isEven without % operator

---
+++ exercises-book-eloquent-javaScript/chapter3/is-even-without-%-recursion.js
@@ -0,0 +1,15 @@
+// interesting version of isEven funcion without use % operator
+
+function isEven(value) 
+{
+  if (value < 0) // if the value is negative, we can turn it on positive 
+    value *= -1;
+  if (value == 0)
+    return true;
+  else if (value == 1)
+    return false;
+  else
+    return isEven(value - 2);
+}
+
+console.log(isEven(-2));
