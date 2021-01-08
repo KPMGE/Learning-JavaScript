cc97e513d0d90ed1bc816c5b8d9a3bddf12fd882
KPMGE <jesuskevin254@gmail.com>
2021-01-08 17:45:32 -0300

    Updating indentation

--- exercises-book-eloquent-javaScript/chapter3/bean-counting.js
+++ exercises-book-eloquent-javaScript/chapter3/bean-counting.js
@@ -1,13 +1,12 @@
 // function that counts the letter occurrency in the string
 
-function countLetter(someStringh, letter) 
-{
+function countLetter(someStringh, letter) {
   let amount = 0;
 
-  for (let i = 0; i < someStringh.length; i++)
-  {
-    if (someStringh[i] === letter)
+  for (let i = 0; i < someStringh.length; i++) {
+    if (someStringh[i] === letter) {
       amount++;
+    }
   }
 
   return amount;
