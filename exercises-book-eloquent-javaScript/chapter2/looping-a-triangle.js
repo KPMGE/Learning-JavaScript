b1d28d6e8a4e4a95161a5a65d6bae31fb6305e31
KPMGE <jesuskevin254@gmail.com>
2021-01-08 17:43:03 -0300

    Updating indentation

--- exercises-book-eloquent-javaScript/chapter2/looping-a-triangle.js
+++ exercises-book-eloquent-javaScript/chapter2/looping-a-triangle.js
@@ -1,9 +1,7 @@
 // Adding first exercise from eloquent JavaScript - charpter 2
 
-for (let i = 0; i < 7; i++)
-{
-  for (let j = 0; j < i; j++)
-  {
+for (let i = 0; i < 7; i++) {
+  for (let j = 0; j < i; j++) {
     console.log("#");
   }
   console.log("\n");
