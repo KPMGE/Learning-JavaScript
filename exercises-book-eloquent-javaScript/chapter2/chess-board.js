cfe03ab63cbc283298f1c5819e8373b5e0d5be0c
KPMGE <jesuskevin254@gmail.com>
2021-01-08 17:42:52 -0300

    Updating indentation

--- exercises-book-eloquent-javaScript/chapter2/chess-board.js
+++ exercises-book-eloquent-javaScript/chapter2/chess-board.js
@@ -3,10 +3,10 @@
 let size = 8;
 let pattern = "# # # #\n";
 
-for (let number = 0; number < size; number++)
-{
-  if (number % 2 != 0)
+for (let number = 0; number < size; number++) {
+  if (number % 2 != 0) {
     console.log(" " + pattern);
-  else 
+  } else {
     console.log(pattern);
+  } 
 }
