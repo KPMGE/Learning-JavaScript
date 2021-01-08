87c42b304211668662436387dc3136989088ddac
KPMGE <jesuskevin254@gmail.com>
2021-01-08 17:42:58 -0300

    Updating indentation

--- exercises-book-eloquent-javaScript/chapter2/fizz-buzz.js
+++ exercises-book-eloquent-javaScript/chapter2/fizz-buzz.js
@@ -1,13 +1,13 @@
 // Adding second exercise from eloquent JavaScript - charpter 2
 
-for (let number = 1; number <= 100; number++)
-{
-  if (number % 3 == 0 && number % 5 == 0)
+for (let number = 1; number <= 100; number++) {
+  if (number % 3 == 0 && number % 5 == 0) {
     console.log("FizzBuzz");
-  else if (number % 3 == 0)
+  } else if (number % 3 == 0) {
     console.log("Fizz");
-  else if (number % 5 == 0)
+  } else if (number % 5 == 0) {
     console.log("Buzz");
-  else 
+  } else {
     console.log(number);
+  } 
 }
