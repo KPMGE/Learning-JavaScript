bd485edf284bcf0c923770e863ded1f23a8b18a6
KPMGE <jesuskevin254@gmail.com>
2021-01-08 08:31:15 -0300

    Adding sample with slice and concat methods

---
+++ arrays/slice-concat.js
@@ -0,0 +1,13 @@
+// slice take elements between 2 values of index
+let arr = [1, 3, 8, 19, 59];
+
+console.log("Original array: ", arr);
+console.log("The values between positions 1 and 4 on array are: ", arr.slice(1, 4));
+console.log("The values of 1 position until the end are: ", arr.slice(1));
+
+
+// concat looks like + operator to strings
+let arr2 = [34, 513];
+
+console.log("Second array: ", arr2);
+console.log("Firs array concatenated with second one: ", arr.concat(arr2));
