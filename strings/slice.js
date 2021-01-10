caa255d987660b6a928561c66f6ca32b2c06b46d
KPMGE <jesuskevin254@gmail.com>
2021-01-10 18:38:58 -0300

    Adding sample with slice method to strings

---
+++ strings/slice.js
@@ -0,0 +1,6 @@
+// the slice method on javaScript is equal to arrays and strings, 
+// it give us the result between two indexes
+
+let name = "Kevin Carvalho de Jesus";
+console.log("Original string: ", name);
+console.log("After use slice: ", name.slice(0, 5));
