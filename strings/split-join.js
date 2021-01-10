ed07352ad26adb9a846f95e82acd11647cf1dd6c
KPMGE <jesuskevin254@gmail.com>
2021-01-10 18:54:18 -0300

    Adding sample of split and join methods on javaScript

---
+++ strings/split-join.js
@@ -0,0 +1,9 @@
+// we can split a string on every occurrence of antother string with 'split method',
+// and join it again with 'join'
+
+let name = "Kevin Carvalho de Jesus";
+let wordsFromName = name.split(" ");
+
+console.log("splitting string into your words: ", wordsFromName);
+console.log("Join it again: ", wordsFromName.join(" "));
+
