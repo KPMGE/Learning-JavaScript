63273c6fa3dfa683153e51b3f6210bffa2077dfa
KPMGE <jesuskevin254@gmail.com>
2021-01-10 19:02:16 -0300

    Adding sample of length property on javaScript

---
+++ strings/length.js
@@ -0,0 +1,8 @@
+// there is a property on every string, it is 'length'
+
+let name = "Kevin Carvalho de Jesus";
+console.log("length of string: ", name.length);
+
+// to access each element from a string, we can use the same notation from arrays
+
+console.log("The first letter from my name is: ", name[0]);
