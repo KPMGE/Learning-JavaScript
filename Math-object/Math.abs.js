da85cb839ff66385df0fbbc187a87b6d79063bad
KPMGE <jesuskevin254@gmail.com>
2021-01-10 21:02:53 -0300

    Adding function that returns the absolute value

---
+++ Math-object/Math.abs.js
@@ -0,0 +1,7 @@
+// we can transform a number to your negative form using Math.abs
+
+let number = -10;
+
+console.log("Original number: ", number);
+console.log("Absolute number: ", Math.abs(number));
+
