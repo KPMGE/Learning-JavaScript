704e7dd8fdd7aa68cf75a65467276fbe68fe9a1f
KPMGE <jesuskevin254@gmail.com>
2021-01-19 11:36:55 -0300

    Adding sample of how to access elements from a matrix

---
+++ arrays/acess-elements-from-matrix.js
@@ -0,0 +1,19 @@
+// we can access the values of a matrix, using two-bracket notation
+
+function multiplyAll(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  return product;
+}
+
+let matrix = [
+  [1, 2],
+  [3, 4],
+  [5, 6, 7]
+];
+
+console.log(multiplyAll(matrix));
