3c51a56405918f4caf1cd2c7cbb3d44984856f18
KPMGE <jesuskevin254@gmail.com>
2021-01-20 12:59:05 -0300

    Adding sample of parseInt function

---
+++ strings/parseInt.js
@@ -0,0 +1,7 @@
+// we can parse a string, converting it to a number using parteInt function
+
+let strNum = "10";
+let parsedNum = parseInt(strNum);
+
+console.log(strNum, typeof (strNum));
+console.log(parsedNum, typeof (parsedNum));
