1400d04606702237d4fc4aab064bbdb13ed9f455
KPMGE <jesuskevin254@gmail.com>
2021-01-29 08:32:21 -0300

    Adding example of how to import/export default functions

---
+++ modules/default-impor-export.js
@@ -0,0 +1,11 @@
+// we can define default functions by using default, to import, we don't need to use {}
+
+// export function
+export default function sum(a, b) {
+  return a + b;
+}
+
+// import function
+import sum from "file.js"
+
+
