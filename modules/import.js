011a934df9619b3157a17a5e17a44ee916a8a48c
KPMGE <jesuskevin254@gmail.com>
2021-01-29 08:28:41 -0300

    Adding sample of how to import functions from another modules

---
+++ modules/import.js
@@ -0,0 +1,6 @@
+// we can import functions from modules by using 'import'
+import { calculateRootsQuadraticFunction } from "./math_functions.js";
+
+let a = 2, b = 3, c = 0;
+
+console.log(calculateRootsQuadraticFunction(a, b, c));
