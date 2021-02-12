270d1490aa7d747cc139ff9ca642e680ee46b68a
KPMGE <jesuskevin254@gmail.com>
2021-02-12 06:50:31 -0300

    Adding sample of method toString

---
+++ arrays/to-string.js
@@ -0,0 +1,9 @@
+// we can convert an array to a string by using the toString method
+
+let days = [21, 12, 31, 5, 6];
+console.log(days);
+
+// converting to string
+let stringDays = days.toString();
+
+console.log(stringDays);
