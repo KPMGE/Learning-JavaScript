807ffd14d2c0f759f426d1c9cbf3cdaa82fd53c0
KPMGE <jesuskevin254@gmail.com>
2021-02-12 06:54:45 -0300

    adding sample of the replace method

---
+++ strings/replace.js
@@ -0,0 +1,7 @@
+// we can replace a certain pattern on a string by using the replace(method)
+
+let name = "Kevin Carvalho de Jesus Santos";
+// replacing 'santos' to a void string
+let realName = name.replace("Santos", "");
+
+console.log(realName);
