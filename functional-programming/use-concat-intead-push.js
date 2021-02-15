be007f1d6a73991a5cc651a445837700420d816d
KPMGE <jesuskevin254@gmail.com>
2021-02-15 16:37:31 -0300

    adding concat method

---
+++ functional-programming/use-concat-intead-push.js
@@ -0,0 +1,7 @@
+// the concat method is more recommended because it doesn't modify original array
+
+let array1 = [2, 31, 62];
+let array2 = [3, 72, 2];
+
+// binding array1 to array2
+console.log(array1.concat(array2));
