f1b949432129fee7688193858cbc298ba83c8fa6
KPMGE <jesuskevin254@gmail.com>
2021-02-15 16:34:56 -0300

    Addding sample with splice

---
+++ functional-programming/using-slice-instead-splice.js
@@ -0,0 +1,7 @@
+// the splice function is more recommended because it doesn't modify the original array
+
+let array = [1, 4, 45, 72, 35, 61];
+
+// displaying 3 first elements
+console.log(array.slice(0, 3));
+console.log(array);
