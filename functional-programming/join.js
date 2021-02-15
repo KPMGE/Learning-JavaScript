fbc6a81a89e46d9aa0ff8924164f6af062e4a6d2
KPMGE <jesuskevin254@gmail.com>
2021-02-15 20:31:51 -0300

    Adding sample of join method

---
+++ functional-programming/join.js
@@ -0,0 +1,8 @@
+// sample of join method
+
+let phrase = ["i", "am", "a", "good", "programmer", "sometimes", "lol"];
+let finalPhrase = phrase.join(" ");
+finalPhrasewithUnderline = phrase.join("_");
+
+console.log(finalPhrase);
+console.log(finalPhrasewithUnderline);
